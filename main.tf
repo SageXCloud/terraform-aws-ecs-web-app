84e5df545473a3c13af20956b3b6d69c8d1485d5
jim-sagekloud <160961989+jim-sagekloud@users.noreply.github.com>
2025-05-26 19:16:44 -0400

    Update ecs_alb_service_task to 0.66.3

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ locals {
 
 module "ecs_alb_service_task" {
   source  = "cloudposse/ecs-alb-service-task/aws"
-  version = "0.66.2"
+  version = "0.66.3"
 
   alb_security_group                 = var.alb_security_group
   use_alb_security_group             = var.use_alb_security_group
